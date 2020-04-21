7a8912d0b668fe66240b7c4b48eb771204454ef1
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-04-21 11:44:13 -0700

    added images to POPOSList

---
+++ src/POPOSList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import POPOSSpace from './POPOSSpace';
+
+function POPOSList() {
+    return (
+        <div>
+            <h1>Awesome header</h1>
+            <POPOSSpace />
+            <POPOSSpace />
+            <POPOSSpace />
+            <POPOSSpace />
+        </div>
+    )
+}
+
+export default POPOSList
---
+++ src/POPOSSpace.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function POPOSSpace() {
+    return (
+        <div>
+            <img src={`${process.env.PUBLIC_URL}images/images/garden-terrace-at-150-california.jpg`} width='300' height='300' alt='Hello' />
+            <h1>Name...</h1>
+            <div>Address...</div>
+        </div>
+    )
+}
+
+export default POPOSSpace
